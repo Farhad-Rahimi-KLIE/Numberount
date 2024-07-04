8ea8f7ea256b7b132f93491fdf3725aa1345c64b
Farhad <Farhadrahimiklie786@gmail.com>
2024-07-04 08:53:01 +0430

    All file uploaded

---
+++ script.js
@@ -0,0 +1,9 @@
+let count = 0;
+function add() {
+  count += 1;
+  document.getElementById("counter").innerHTML = count;
+}
+function lower() {
+  count -= 1;
+  document.getElementById("counter").innerHTML = count;
+}
